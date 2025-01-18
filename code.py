dbf1bc3a7c5397f825375e1e96a62e35893cdfca
amrita09-pix <amrandal09@gmail.com>
2025-01-18 14:57:49 +0530

    second version of data saved

--- code.py
+++ code.py
@@ -13,4 +13,7 @@ df = pd.DataFrame(data)
 data_dir = "data"
 os.makedirs(data_dir,exist_ok =True)
 file_path = os.path.join(data_dir,"sample_data.csv")
+#df.to_csv(file_path,index=False)
+new_row = {'name':'Dylan','age':'40','city':'Paris'}
+df.loc[len(df.index)] = new_row
 df.to_csv(file_path,index=False)
